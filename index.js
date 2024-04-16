c0e5f097c4c98e78f8cacac8baa66d345bbe6ae4
T-Prakash2002 <prakasht1405@gmail.com>
2024-04-16 23:10:08 +0530

    fs method

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
+const fs=require('fs')
 
-const {MongoClient}=require('mongodb')
 
-const uri=`mongodb+srv://prakasht1405:MongoDB145@cluster0.korygfe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-const client=new MongoClient(uri);
-
-async function main(){
-    try {
-    
-        await client.connect()
-        console.log("connected DB");
-
-        const database=client.db('sample_mflix');
-        const collection=database.collection('users')
-        
-        const result=await collection.find({}).toArray();
-
-        console.log(result.map(item=>item.name));
-        
-        
-        } 
-        finally{
-            await client.close();
-        }
-}
-
-main().catch((err)=>{
-    console.log(err);
+fs.stat('file.txt',(err,stats)=>{
+    if(err){
+        console.log('Error');
+    }console.log(stats);
 })
