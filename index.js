05a0bcdf9c348c81c6b2112b7841f8a8ea1f24ab
T-Prakash2002 <prakasht1405@gmail.com>
2024-04-16 23:01:58 +0530

    nodejs to mongoDB connectivity

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hello world");
+
+const {MongoClient}=require('mongodb')
+
+const uri=`mongodb+srv://prakasht1405:MongoDB145@cluster0.korygfe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+const client=new MongoClient(uri);
+
+async function main(){
+    try {
+    
+        await client.connect()
+        console.log("connected DB");
+
+        const database=client.db('sample_mflix');
+        const collection=database.collection('users')
+        
+        const result=await collection.find({}).toArray();
+
+        console.log(result.map(item=>item.name));
+        
+        
+        } 
+        finally{
+            await client.close();
+        }
+}
+
+main().catch((err)=>{
+    console.log(err);
+})
