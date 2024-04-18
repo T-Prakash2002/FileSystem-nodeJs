7c2bb2e6c5c1896109685d9a736ce324e297873f
T-Prakash2002 <prakasht1405@gmail.com>
2024-04-18 18:31:23 +0530

    task file included

--- index.js
+++ index.js
@@ -1,23 +1,64 @@
-const fs=require('node:fs')
+const fs=require('fs')
+const express=require('express')
 
 
+const app=express();
 
 
-try {
+app.get('/', (req, res)=>{
+
+    //Sending response status
+    res.status(200).send(`
+
+            <div style="border:1px solid black">
+
+            <h1 
+            style="padding:10px 10px;text-align:center;background-color:gray;">
+            Express Server is Connected !
+            </h1>
+            <div style="text-align:center">
+            <p>
+            <span style="font-size:18px"> 
+                To Create a New txt file
+            </span> --> <br>
+            <button style="border:2px solid Tomato;font-size:18px;border-radius:9px;">
+                <a style="text-decoration:none" href="/new-file">
+                    Click Here
+                </a>
+            </button>
+            </p>
+            <p>
+            <span style="font-size:18px">
+                Show All txt file
+                </span> --> <br>
+                <button style="border:2px solid Tomato;font-size:18px;border-radius:9px;">
+            <a style="text-decoration:none;" href="/read-all-files">
+                Click Here
+            </a>
+            </button>
+            </p>
+
+            </div></div>
+    `);
     
-    // fs.readFile('file.txt','utf-8',(err,data)=>{
-    //     
-    //     console.log(data);
-    //     
-    // })
+});
 
-    text='modified'
-
-    fs.writeFile('file.txt',text,err=>{
-        err?console.log(err):console.log('sucessfull');;
-    })
+app.get('/new-file',(res,req)=>{
+    
+    
+})
 
 
-} catch (error) {
-    console.log(error);
-}
+app.get('/read-all-files',(res,req)=>{
+
+})
+
+
+
+
+
+
+app.listen(4000,()=>{
+    console.log(`Server Running success in Port 4000`);
+})
+
